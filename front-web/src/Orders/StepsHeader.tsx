8f667e21ae98076540b344c0e8227653ce4cdc3b
Henrique Lopes <henriqueborsoilopes@gmail.com>
2021-01-08 23:55:37 -0300

    Search order location

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 function StepsHeader() {
     return (
         <header className="orders-steps-container">
-            <div className="orders=steps-coonten">
+            <div className="orders-steps-content">
                 <h1 className="steps-title">
                     SIGA AS <br /> ETAPAS
                 </h1>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
 import './styles.css';
 import './StepsHeader.tsx';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { OrderLocationData, Product } from './types';
+import { fetchProducts } from './Api';
+import OrderLocation from './OrderLocation.tsx';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderlocation, setOrderLocation] = useState<OrderLocationData>();
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch()
+    }, []);
     return (
        <div className="orders-container">
            <StepsHeader />
+           <ProductsList products={products} />
+           <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
        </div>
     )
 }
